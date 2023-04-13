cf81ebb201b4f10bdc8e35f545798c148a678ecd
songyunjeong <yungunaa@naver.com>
2023-04-13 11:41:30 +0900

    shoes 데이터 사용하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import './App.css';
+import data from './data';
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className='App'>
+      <img
+        src={process.env.PUBLIC_URL + '/imgs/logo.png'}
+        alt='로고 이미지'
+        className='logo-img'
+      />
+
       <Navbar
         style={{ backgroundColor: '#ee1c25', fontWeight: 'bold' }}
         variant='dark'
@@ -19,7 +29,30 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
+      <img
+        className='main-bg'
+        src={process.env.PUBLIC_URL + '/imgs/bg.jpg'}
+        alt='배경 이미지'
+      ></img>
+
+      <Container>
+        <Row>
+          {shoes.map((data, i) => {
+            return (
+              <Col sm>
+                <img
+                  src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`}
+                  alt='배경 이미지'
+                  width='80%'
+                />
+                <h4>{shoes[i].title}</h4>
+                <p>{shoes[i].content}</p>
+                <p>{shoes[i].price.toLocaleString()}</p>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
