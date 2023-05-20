315d5e491d782f005b52489603bf3446047f6103
송윤정 <yungunaa@naver.com>
2023-05-20 17:20:59 +0900

    lazy import, Suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, lazy, Suspense, useEffect, useState } from "react";
 import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import "./App.css";
 import data from "./data";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import Detail from "./pages/Detail";
-import Event from "./pages/Event";
 import axios from "axios";
 import Loading from "./components/Loading";
-import Cart from "./pages/Cart";
 import { useQuery } from "@tanstack/react-query";
 
 export let Context = createContext();
+
+const Detail = lazy(() => import("./pages/Detail"));
+const Event = lazy(() => import("./pages/Event"));
+const Cart = lazy(() => import("./pages/Cart"));
 
 function App() {
   useEffect(() => {
@@ -108,91 +109,95 @@ function App() {
         </Navbar>
       </header>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <img
-                className="main-bg"
-                src={process.env.PUBLIC_URL + "/imgs/bg.jpg"}
-                alt="배경 이미지"
-                style={{
-                  width: "100%",
-                  height: "auto",
-                  maxWidth: "1320px",
-                  marginBottom: "30px",
-                }}
-              ></img>
+      <Suspense fallback={<div>로딩중</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <img
+                  className="main-bg"
+                  src={process.env.PUBLIC_URL + "/imgs/bg.jpg"}
+                  alt="배경 이미지"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    maxWidth: "1320px",
+                    marginBottom: "30px",
+                  }}
+                ></img>
 
-              <Container>
-                <Row>
-                  {shoes.map((data, i) => {
-                    return (
-                      <Card
-                        key={i}
-                        navigate={navigate}
-                        shoes={shoes[i]}
-                        i={i}
-                      />
-                    );
-                  })}
-                </Row>
-                {loding && <Loading />}
-                {clickCount === 2 ? (
-                  <div style={{ padding: "30px" }}>더이상 상품이 없습니다.</div>
-                ) : (
-                  <button
-                    onClick={() => {
-                      setClickCount(clickCount + 1);
-                      setLoding(true);
-                      axios
-                        .get("https://codingapple1.github.io/shop/data2.json")
-                        .then((result) => {
-                          let copy = [...shoes, ...result.data];
-                          if (clickCount < 1) {
-                            setShoes(copy);
-                          }
-                          setLoding(false);
-                        })
-                        .catch(() => {
-                          setLoding(false);
-                          console.log("error");
-                        });
-                    }}
-                    style={{
-                      border: "none",
-                      fontSize: "14px",
-                      padding: "5px 10px",
-                      margin: "20px",
-                    }}
-                  >
-                    MORE
-                  </button>
-                )}
-              </Container>
-            </>
-          }
-        />
-        <Route path="/men" element={<div>MEN 페이지</div>} />
-        <Route path="/women" element={<div>WOMEN 페이지</div>} />
-        <Route path="/kids" element={<div>KIDS 페이지</div>} />
-        <Route path="/sale" element={<div>SALE 페이지</div>} />
-        <Route path="/event" element={<Event />}>
-          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
-          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
-        </Route>
-        <Route
-          path="/detail/:id"
-          element={
-            <Context.Provider value={{ stock }}>
-              <Detail shoes={shoes} />
-            </Context.Provider>
-          }
-        />
-        <Route path="/cart" element={<Cart>장바구니</Cart>} />
-        <Route path="*" element={<div>없는 페이지</div>} />
-      </Routes>
+                <Container>
+                  <Row>
+                    {shoes.map((data, i) => {
+                      return (
+                        <Card
+                          key={i}
+                          navigate={navigate}
+                          shoes={shoes[i]}
+                          i={i}
+                        />
+                      );
+                    })}
+                  </Row>
+                  {loding && <Loading />}
+                  {clickCount === 2 ? (
+                    <div style={{ padding: "30px" }}>
+                      더이상 상품이 없습니다.
+                    </div>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        setClickCount(clickCount + 1);
+                        setLoding(true);
+                        axios
+                          .get("https://codingapple1.github.io/shop/data2.json")
+                          .then((result) => {
+                            let copy = [...shoes, ...result.data];
+                            if (clickCount < 1) {
+                              setShoes(copy);
+                            }
+                            setLoding(false);
+                          })
+                          .catch(() => {
+                            setLoding(false);
+                            console.log("error");
+                          });
+                      }}
+                      style={{
+                        border: "none",
+                        fontSize: "14px",
+                        padding: "5px 10px",
+                        margin: "20px",
+                      }}
+                    >
+                      MORE
+                    </button>
+                  )}
+                </Container>
+              </>
+            }
+          />
+          <Route path="/men" element={<div>MEN 페이지</div>} />
+          <Route path="/women" element={<div>WOMEN 페이지</div>} />
+          <Route path="/kids" element={<div>KIDS 페이지</div>} />
+          <Route path="/sale" element={<div>SALE 페이지</div>} />
+          <Route path="/event" element={<Event />}>
+            <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+            <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+          </Route>
+          <Route
+            path="/detail/:id"
+            element={
+              <Context.Provider value={{ stock }}>
+                <Detail shoes={shoes} />
+              </Context.Provider>
+            }
+          />
+          <Route path="/cart" element={<Cart>장바구니</Cart>} />
+          <Route path="*" element={<div>없는 페이지</div>} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
